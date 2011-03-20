fd6157bd72ef0b682eb4996599d58a6004e9d05f
Alexey <leopard.not.a@gmail.com>
2011-03-20 16:29:17 +0200

    Finished

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.media a').lightBox({
+      imageLoading: '/images/lightbox/lightbox-ico-loading.gif',
+      imageBtnPrev: '/images/lightbox/lightbox-btn-prev.gif',
+      imageBtnNext: '/images/lightbox/lightbox-btn-next.gif',
+      imageBtnClose: '/images/lightbox/lightbox-btn-close.gif',
+      imageBlank: '/images/lightbox/lightbox-blank.gif'
+    });
+});
