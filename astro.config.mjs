b0f9c4832f1811708969cea51a142933afe21f61
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-05-11 16:32:24 +0300

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
       xslURL: "/rss/sitemap.xsl",
       changefreq: ChangeFreqEnum.WEEKLY,
       priority: 0.7,
-      lastmod: new Date()
+      lastmod: new Date().toISOString()
     }),
   ],
   markdown: {
