7dee19b9001f1e5224fa69dc549934aa47610d29
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-05-17 14:00:35 +0300

    update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -11,6 +11,19 @@ export async function GET(context) {
     title: siteConfig.title,
     description: siteConfig.description,
     site: context.site,
+    xmlns: {
+      creativeCommons: 'http://backend.userland.com/creativeCommonsRssModule',
+      atom: 'http://www.w3.org/2005/Atom',
+      content: 'http://purl.org/rss/1.0/modules/content/'
+    },
+    customData: [
+      '<language>uk-UA</language>',
+      // atom
+      `<atom:link href="${new URL("rss.xml", import.meta.env.SITE)}" rel="self" type="application/rss+xml" />`,
+      `<atom:link href="${import.meta.env.SITE}" rel="hub" xmlns="http://www.w3.org/2005/Atom" />`,
+      // creative common
+      '<creativeCommons:license>https://creativecommons.org/licenses/by/4.0/</creativeCommons:license>'
+    ].join(''),
     // Blog entries
     items: posts.map((post) => ({
       ...post.data,
